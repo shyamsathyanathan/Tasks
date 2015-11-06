48c939e8da20273c4fa07de3c2b536241002f080
shyamsathyanathan <shyamsathyanathan@yahoo.in>
2015-11-06 22:25:03 +0530

    Added basic task management logic to MainCtrl and updated bower dependencies to include angular-local-storage

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
 var tasksApp = angular.module('Tasks', []);
 
-tasksApp.controller('MainCtrl', function() {
+tasksApp.factory('TaskService', ['localStorageService', function(localStorageService) {
   
-});
+  var service = {};
+  
+  service.getTasksList = function() {
+    return localStorageService.get('tasks');
+  };
+  
+  service.saveTask = function(task) {
+    var existingTasks = service.getTasksList();
+    if(existingTasks) {
+      existingTasks.push(task);
+      localStorageService.set('tasks', existingTasks);
+    } else {
+      var newTaskList = [];
+      newTaskList.push(task);
+      localStorageService.set('tasks', newTaskList);
+    }
+  };
+  
+  service.removeTask = function(taskPosition) {
+    var tasksList = service.getTasksList();
+    
+    taskList = taskList.splice(taskPosition, 1);
+    
+    localStorageService.set('tasks', taskList);
+  };
+  
+  return service;
+  
+}]);
+
+tasksApp.controller('MainCtrl', ['$scope', 'TaskService', function($scope, TaskService) {
+  
+  var controller = this;
+  
+  $scope.task = {};
+  
+  controller.updateTaskList = function() {
+    $scope.tasks = TaskService.getTasksList() || [];
+  };
+  
+  controller.createTask = function() {
+    if($scope.task !== {}) {
+      $scope.task.createdTime = new Date();
+      $scope.task.markedAsDone = false;
+      TaskService.saveTask($scope.task);
+      controller.updateTaskList();
+    }
+  };
+  
+}]);
